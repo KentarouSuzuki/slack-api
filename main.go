fee9b59e2ecc07cf58a6e50ddb35f39b43d38656
鈴木健太郎 <suzukikentarou@suzukikentarounoiMac.local>
2019-05-04 17:25:32 +0900

    [feat]initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/KentarouSuzuki/slack-member/config"
+)
+
+func main() {
+	Conf, Err := config.Get()
+	fmt.Println(Err)
+	fmt.Println(Conf.DataBase.Host)
+	fmt.Println(Conf.Slack.Token)
+}
